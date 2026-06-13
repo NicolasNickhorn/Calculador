9155aa1a12875c5d7252ddffe1443ad743046ace
Nicolas <142843786+NicolasNickhorn@users.noreply.github.com>
2026-06-13 16:41:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const setDisplay = document.getElementById("display");
 
 function setNumbers(number) {
-    setDisplay.textContent += number;
+    setDisplay.textContent += number;  
 }
 
 function setOperator(operator) {
@@ -25,3 +25,57 @@ function calcResult() {
         setDisplay.textContent = "Error"
     }
 }
+
+function squareRoot(){
+    setDisplay.textContent = Math.sqrt(setDisplay.textContent);
+}
+
+function eraseLast(){
+    setDisplay.textContent = setDisplay.textContent.slice(0, -1);
+}
+
+
+
+document.addEventListener("keydown", (event) =>{
+
+    if (event.key === "/") {
+        event.preventDefault();
+    }
+
+    switch(event.key){
+        case "Enter":
+            calcResult();
+            break;
+
+        case "Backspace":
+            eraseLast();
+            break;
+
+        case "c":
+        case "C":
+        case "Escape":
+            clearDisplay();
+            break;
+
+        case "+":
+        case "-":
+        case "*":
+        case "/":
+            setOperator(event.key);
+            break;
+
+        case ".":
+            setNumbers(".");
+            break;
+
+        case "r":
+        case "R":
+            squareRoot();
+            break;
+
+        default:
+            if(!isNaN(event.key)){
+                setNumbers(event.key);
+            }
+    }
+})
